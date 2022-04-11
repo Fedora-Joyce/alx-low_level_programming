49b9795da2fb0c654a7e3b1b1cbdfe7dee5401b2
Fedora-Joyce <fedorajoyce2@gmail.com>
2022-04-11 01:28:16 -0700

    My task 102

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -14,6 +14,7 @@ for (num1 = 0; num1 <= 98; num1++)
 {
 for (num2 = num1 + 1; num2 <= 99; num2++)
 {
+
 putchar((num1 / 10) + '0');
 putchar((num1 % 10) + '0');
 putchar(' ');
@@ -21,7 +22,8 @@ putchar((num2 / 10) + '0');
 putchar((num2 % 10) + '0');
 
 if (num1 == 98 && num2 == 99)
-	continue;
+continue;
+
 putchar(',');
 putchar(' ');
 }
@@ -30,4 +32,7 @@ putchar(' ');
 putchar('\n');
 
 return (0);
+
+
 }
+
