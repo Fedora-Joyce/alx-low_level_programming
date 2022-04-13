fcb4f2ac778b1a7a0a620efc4f8975fb273d2b2b
Fedora-Joyce <fedorajoyce2@gmail.com>
2022-04-13 00:56:53 -0700

    my task 6

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -4,22 +4,27 @@
 /**
  * _abs - value absolute
  *
- * @n: -value absolute
- *Return: always 0.
-/
-int _abs(int n < 0)
-{
-return (n * (-1));
-}
+ * @n: the int to print
+ * Return: always 0.
+ */
 
-else if (n == 0)
+int _abs(int n)
 {
-return (0);
-}
 
-else
-{
-return (n);
-}
+	if (n < 0)
+	{
+		return (n * (-1));
+	}
 
-}
+	else if (n == 0)
+	{
+		return (0);
+
+		else
+		{
+			return (n);
+		}
+
+	}
+
+
