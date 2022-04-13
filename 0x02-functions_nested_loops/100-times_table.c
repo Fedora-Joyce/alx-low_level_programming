d4d543fec713fcb946639826a8141070080ac779
Fedora-Joyce <fedorajoyce2@gmail.com>
2022-04-13 07:19:24 -0700

    my task 12

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,5 +1,7 @@
 #include "main.h"
-/**print_times_table - prints the times table of the input,
+
+/**
+ * print_times_table - prints the times table of the input,
  * 			starting with 0.
  * @n: the value of the times table to be printed
  */
