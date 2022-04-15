f729621acb44d66613e29a15ca1da6a4324a784b
Fedora-Joyce <fedorajoyce2@gmail.com>
2022-04-15 15:55:32 -0700

    My commit message for task 1

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
-
 /**
  * main - causes an infinite loop
+ *
  * Return: 0
  */
 
@@ -13,10 +13,13 @@ int main(void)
 
 	i = 0;
 
-	while (i < 10)
-	{
-		putchar(i);
+	/**
+	 * while (i < 10)
+	 * {
+	 * infinite loop - No increase of variables
+	 * putchar(i);
 	}
+*/
 
 	printf("infinite loop avoided! \\o/\n");
 
