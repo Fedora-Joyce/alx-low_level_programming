2f03a2c2f7f53169ccd6a70f127d1249c740fedf
Fedora-Joyce <fedorajoyce2@gmail.com>
2022-04-15 17:08:47 -0700

    My zero task

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -2,14 +2,14 @@
 
 /**
  * main - tests function that prints if integer is positive or negative
- * return: zero
+ *Return: 
  */
 
 int main(void)
 {
 	int i;
-
 	i = 0;
+
 	positive_or_negative(i);
 
 	return (0);
