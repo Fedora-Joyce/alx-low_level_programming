41aaba83ef4b588fa316e93dd643a8591715b647
Fedora-Joyce <fedorajoyce2@gmail.com>
2022-04-20 13:11:12 -0700

    my task 4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_rev - prints a string in reverse order
+ * @s:  a pointer to an int that will change
+ *
+ * Return: void which means correct answer
+ */
+
+void print_rev(char *s)
+{
+	int i;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+
+	for i = i - 1; i >= 0; i--)
+	{
+		_putchar (s[i]);
+	}
