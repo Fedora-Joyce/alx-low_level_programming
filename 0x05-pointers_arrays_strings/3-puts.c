972489b657cfb24741c8bf8e96d0a51eb77d6181
Fedora-Joyce <fedorajoyce2@gmail.com>
2022-04-20 13:00:03 -0700

    my task 3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _puts - print a string
+ * @str: a pointer to an integer that will be chnaged
+ *
+ * return: void which means correct answer
+ */
+
+void _puts(char *str)
+{
+	char *c;
+	int m;
+
+	c = str;
+
+	for (m = 0; c[m]; m++)
+	{
+		_putschar (c[m]);
+	}
+	_putschar('\n')
+}
