7085957489845fbb7993923d84bee00778a6486b
Fedora-Joyce <fedorajoyce2@gmail.com>
2022-04-25 15:41:20 -0700

    task 2

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,13 +1,12 @@
 #include "main.h"
-
 /**
- * _strncpy - a function that copies a string.
- * at most n bytes of src are copied.
- * if length of src is < n, additional null bytes are written to dest
- * to ensure that a total of n bytes are written.
- * @dest: pointer to destination string.
- * @src: pointer to source string.
- * @n: number of bytes to be used.
+ *_strncpy - a function that copies a string.
+ *at most n bytes of src are copied.
+ *if length of src is < n, additional null bytes are written to dest
+ *to ensure that a total of n bytes are written.
+ *@dest: pointer to destination string.
+ *@src: pointer to source string.
+ *@n: number of bytes to be used.
  *
  * Return: pointer to destination string.
  */
@@ -17,9 +16,9 @@ char *_strncpy(char *dest, char *src, int n)
 int byteCount;
 
 
-for(byteCount = 0; byteCount < n && src[byteCount] != '\0'; byteCount++)
+for (byteCount = 0; byteCount < n && src[byteCount] != '\0'; byteCount++)
 dest[byteCount] = src[byteCount];
-for(; byteCount < n; byteCount++)
+for (; byteCount < n; byteCount++)
 dest[byteCount] = '\0';
 
 return (dest);
