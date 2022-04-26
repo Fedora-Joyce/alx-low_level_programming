7c5bc4adb6c1062dcabe8dd9a108a73b12eb34bf
Fedora-Joyce <fedorajoyce2@gmail.com>
2022-04-26 01:19:15 -0700

    task 2

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -15,7 +15,6 @@ char *_strncpy(char *dest, char *src, int n)
 {
 int byteCount;
 
-
 for (byteCount = 0; byteCount < n && src[byteCount] != '\0'; byteCount++)
 dest[byteCount] = src[byteCount];
 for (; byteCount < n; byteCount++)
