f322038b584ec4eacdd508f6cc0ec121a01f40c7
Fedora-Joyce <fedorajoyce2@gmail.com>
2022-04-27 11:04:27 -0700

    task 0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,25 +1,29 @@
 #include "main.h"
 
 /**
- * _strcat -function that concatenates
- * 		two strings.
- * @dest: pointer to destination input
- * @src: pointer to source input
+ * _strcat - appends the src string to the dest string,
+ * overwriting the terminating null byte(\0) at the end
+ * of dest, and adds a terminating null byte.
+ * @dest: the destination.
+ * @src: the source to be concatenated to @dest.
  *
- * Return: pointer to resulting string @dest
+ * Return: dest.
  */
 
 char *_strcat(char *dest, char *src)
 {
-int c, c2;
-c = 0;
-/*find the size of dest array*/
-while (dest[c])
-c++;
-/* iterate through each src array value without the null byte*/
-for (c2 = 0; src[c2]; c2++)
-/* append src[c2] to dest[c] while overwritting the null byte in dest*/
-dest[c++] = src[c2];
-return (dest);
+	int len = 0;
+	char *cpy;
+
+	for (; dest[len] != '\0'; len++)
+		;
+
+	cpy = dest + len;
+	while (*src != '\0')
+		*cpy++ = *src++;
+	*cpy = '\0';
+
+	return (dest)
 }
+
 
