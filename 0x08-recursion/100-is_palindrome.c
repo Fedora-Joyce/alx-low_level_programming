3a5c6636f1d9a7b2e171d11de4ddbeb51f3717bb
Fedora-Joyce <fedorajoyce2@gmail.com>
2022-04-29 09:37:50 -0700

    task 7

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -42,9 +42,10 @@ int p1(char *s, int l)
  * Return: recursion
  */
 
-int is_palindrome(char *s int l)
+int is_palindrome(char *s)
 {
 	int len = _strlen_recursion(s);
+
 	return (p1(s, len - 1));
 }
 
