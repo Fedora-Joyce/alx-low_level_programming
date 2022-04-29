35ad32b1f803345907f7028fff2627a00a41ad8a
Fedora-Joyce <fedorajoyce2@gmail.com>
2022-04-29 02:09:15 -0700

    task 6

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,55 +1,51 @@
 #include "main.h"
 
 /**
- *evaluate_num - recursion loop
- *@num; num
- *@iterator: number to iterate
- *Return: return 1 or 0
+ *is_prime_number - determine if number is prime
+ *@n: number
+ *Return: 1 if prime, 0 otherwise
  */
 
-int evaluate_num(int num, int iterator)
+int is_prime_number(int n)
 {
-
-if (iterator == num -1)
+	if (n < 2)
+		return (0);
+	if (n < 4)
+		return (1);
+	return (prime(n, 2));
 {
-return (1);
-}
-
-else if (num % iterator == 0)
-{
-return (0);
-}
-
-if (num % iterator != 0)
-{
-return (evaluate_num(num, iterator + 1));
-}
-
-return (0);
-}
-
-/**
- *is_prime_number - evaluate prime or not
- *@num: number
- *Return: return 1 prime - return 0 otherwise
+/
+ * my_sqrt - returns sqrt of number
+ * @x: number
+ * @i: number acting as divisor
+ * Return: square root of x
  */
-int is_prime_numbe(int num)
-{
 
-int iterator;
+ int my_sqrt(int x, int i)
+ {
+ int square
 
-iterator = 2;
+square = i*i;
+if(square >= x)
+       return (i);
+else
+	return (my_sqrt(x, i + 1));
+ }
 
-/* only greater than 2*/
-if (num < 2)
-{
-return (0);
+ /**
+  * prime - helper function to find prime
+  * @n: number
+  * @d: incrementor divisor
+  * Return: 0 if not prime, 1 if prime
+  */
+ int prime(int n, int d)
+ {
+	 if (n % d == 0)
+		 return (0);
+	 else if (prime(n, 1) < d)
+		 return (1);
+	 else
+		 return(prime(n, d + 1));
 }
 
-if (num == 2)
-{
-return (1);
-}
 
-return (evaluate_num(num, iterator));
-100-is_palindrome.c
