a6b340f2398a198f7ca2c36a0d1a344b8ff5f801
Fedora-Joyce <fedorajoyce2@gmail.com>
2022-06-12 16:48:59 -0700

    task 9

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,53 +1,55 @@
 #include "lists.h"
 
 /**
- * insert_nodeint_at_index - a function that inserts a new node at
- *                           a given position
+ * insert_nodeint_at_index - inserts a new node at idx of list listint_t
  *
- * @head: pointer to the first node of the list
- * @idx: is the index of the list where the new node should be added
- * @n: element to add to the new node
+ * @head: pointer to head element
+ * @idx: the index to insert at
+ * @n: the int data to insert
  *
- * Return: NULL if anything fails or the address of the new node
-*/
+ * Return: a pointer to the new node
+ *
+ */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	listint_t *new_node, *current;
-	unsigned int index;
-
-	current = *head; /*place first node at current*/
-
-	new_node = malloc(sizeof(listint_t));
-	if ((*head == NULL && idx != 0) || new_node == NULL)
-		return (NULL);
-
-	new_node->n = n; /* add our element to the new node*/
-
-	/*iterate list to node position idx - 2*/
-	for (index = 0; head != NULL && index < idx - 1; index++)
-	{
-		current = current->next;
-		if (current == NULL)
-			return (NULL);
-	}
-
-	if (idx == 0) /*if the index for new node is 0*/
-	{
-		/*first node will be moved to second node*/
-		new_node->next = *head;
-		/*new node will be placed as the first node*/
-		*head = new_node;
-	}
-	else if (current->next) /*if index where to add our new node is not 0*/
-	{
-		new_node->next = current->next; /*place current node after new node*/
-		current->next = new_node;/*set the new node at index idx*/
-	}
-	else /*if node position is not present in the list*/
-	{
-		new_node->next = NULL;/*set next addr as NULL, indicates last node*/
-		current->next = new_node;/*set the new node at the last position in list*/
-	}
-
-	return (new_node);
+listint_t *temp, *prev_n, *new;
+unsigned int counter = 0;
+if ((void *)head == NULL)
+{
+return (NULL);
+}
+new = malloc(sizeof(listint_t));
+if (new == NULL)
+{
+return (NULL);
+}
+temp = *head;
+prev_n = *head;
+new->n = n;
+new->next = NULL;
+if (idx == 0)
+{
+new->next = temp;
+*head = new;
+return (new);
+}
+while (temp != NULL)
+{
+if (counter == idx)
+{
+prev_n->next = new;
+new->next = temp;
+return (new);
+}
+counter++;
+prev_n = temp;
+temp = temp->next;
+}
+if (counter == idx)
+{
+prev_n->next = new;
+return (new);
+}
+free(new);
+return (NULL);
 }
