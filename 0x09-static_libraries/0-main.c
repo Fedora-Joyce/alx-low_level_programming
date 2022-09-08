8407f535bcc4e96d9d2668a0aa743c37057458b9
Fedora-Joyce <fedorajoyce2@gmail.com>
2022-09-08 04:15:14 -0700

    Updates

---
+++ 0x09-static_libraries/0-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - entry check alx code
+ *
+ * Return: always 0 (success)
+ */
+
+int main(void)
+{
+	_puts("\"At the end of the day, my goal was to be the best hacker\"\n\t- kevin Mitnick");
+	return (0);
+}
+
+
---
+++ 0x09-static_libraries/1-memcpy.c
@@ -0,0 +1,18 @@
+/**
+ * _memcpy - function that copies memory area
+ *
+ * @dest: buffer where we will copy to
+ * @src: what are we to copy
+ * @n: n bytes of @src
+ *
+ * Return: always 0 success
+ */
+{
+	unsigned= ;i < n; i++)
+	       dest[i] = src[i];
+return (dest);
+}int i;
+
+	for (i = 0
+char *_memcpy(char *dest, char *src, unsigned int n)
+
